c8e63abecb35f7ef0b3ee5368d0853a562dff833
Aleksei Alekseev <alexeev.mail@gmail.com>
2025-11-13 21:29:33 +0100

    Respond to PING

--- app/main.js
+++ app/main.js
@@ -1,10 +1,15 @@
 const net = require("net");
+const { Buffer } = require('node:buffer');
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-const server = net.createServer((connection) => {
-//   // Handle connection
+let buffer = Buffer.alloc(0);
+
+const server = net.createServer((socket) => {
+    socket.on('data', (chunk) => {
+        socket.write('+PONG\r\n');
+    })
 });
 
 server.listen(6379, "127.0.0.1");
